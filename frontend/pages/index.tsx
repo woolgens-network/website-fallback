a67f8afefaff19bbc8bd2966ded9ddf5d78d5e73
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-19 21:53:38 +0100

    Attempted to add page as fallback server again

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,14 @@
-import CountDown from "../components/CountDown";
+import Maintenance from "../components/Maintenance";
 
 const Page = () => {
-    return <CountDown />
+    return <Maintenance />
+}
+
+export async function getStaticProps() {
+    return {
+        props: {}, // will be passed to the page component as props
+        notFound: true
+    }
 }
 
 export default Page
