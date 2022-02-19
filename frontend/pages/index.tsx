a6b91383f2f04c6ea0026fa3e592c2a985472c60
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-19 22:07:43 +0100

    Added new Node page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,11 +4,4 @@ const Page = () => {
     return <Maintenance />
 }
 
-export async function getStaticProps() {
-    return {
-        props: {}, // will be passed to the page component as props
-        notFound: true
-    }
-}
-
 export default Page
